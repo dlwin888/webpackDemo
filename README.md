acc5dbbba5b015dd419aa663c4c73ac392fba8fb
dlwin888 <duanle888@gmail.com>
2018-03-31 00:52:28 +0800

    webpack loader使用

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # webpackDemo
 webpack学习
+参考：
+https://blog.zfanw.com/webpack-tutorial/
--- src/index.js
+++ src/index.js
@@ -1,4 +1,10 @@
-document.write("HELLO WORLD!");
-console.log('hello js!!');
-
-
+import React from 'react'
+import ReactDOM from 'react-dom'
+import icon from './img/gachip035-23.jpg'
+import './index.css'
+class App extends React.Component {
+  render() {
+    return <div>hello webpack~!<img src={icon} /></div>
+  }
+}
+ReactDOM.render(<App />, document.getElementById('app'))
