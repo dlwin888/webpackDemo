f794ee3c3bf9537f5f13fab9570f7f926aca589f
dlwin888 <duanle888@gmail.com>
2018-04-02 21:37:59 +0800

    update title

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import icon from './img/gachip035-23.jpg'
 import './index.css'
 class App extends React.Component {
   render() {
-    return <div>hello webpack~!<img src={icon} /></div>
+    return <div><h2 style={{color:'#fff'}}>hello webpack~!</h2><img src={icon} /></div>
   }
 }
 ReactDOM.render(<App />, document.getElementById('app'))
